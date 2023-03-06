6b896bbe78f544761d5243cedef35adfa9a78290
fteutsch <florian.teutsch@inovex.de>
2023-03-06 22:29:22 +0100

    Add new datasets and cluster algorithms

--- cluster_vis.py
+++ cluster_vis.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List, Dict
 
 import streamlit as st
 import numpy as np
@@ -9,8 +10,8 @@ from sklearn import cluster, mixture
 
 
 class ClusterAlgo(str, Enum):
-    KMEANS = "kmeans"
-    DBSCAN = "dbscan"
+    KMEANS = "KMeans"
+    DBSCAN = "DBSCAN"
     MEAN_SHIFT = "mean_shift"
     WARD = "ward"
     AGGLOMERATIVE_CLUSTERING = "agglomerative_clustering"
@@ -18,7 +19,7 @@ class ClusterAlgo(str, Enum):
     OPTICS = "optics"
     AFFINITY_PROPAGATION = "affinity_propagation"
     BIRCH = "birch"
-    GAUSSIAN_MIXTURE = "gaussian_mixture"
+    GAUSSIAN_MIXTURE = "Gaussian Mixture"
 
 
 # TODO: Customize icon
@@ -93,21 +94,33 @@ def main():
 
     # Default Data
     n_samples = 500
+    n_features = 3 if is_3d else 2
     data_method = st.sidebar.selectbox(
-        "Default Data", ["Blobs", "Circles", "Moons", "No Structure", "Varied Variances"])
+        "Default Data", ["Blobs", "Circles", "Moons", "No Structure", "Varied Variances", "Anisotropicly distributed"])
     if data_method == "Blobs":
         default_cluster_features, default_cluster_labels = make_blobs(n_samples=n_samples, centers=DEFAULT_N_CLUSTERS,
-                                                                      n_features=3 if is_3d else 2, random_state=6)
+                                                                      n_features=n_features, random_state=6)
     elif data_method == "Circles":
         default_cluster_features, default_cluster_labels = make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=6)
+        if is_3d:
+            default_cluster_features = np.insert(default_cluster_features, 2, 1, axis=1)
     elif data_method == "Moons":
         default_cluster_features, default_cluster_labels = make_moons(n_samples=n_samples, noise=0.05)
+        if is_3d:
+            default_cluster_features = np.insert(default_cluster_features, 2, 1, axis=1)
     elif data_method == "No Structure":
-        default_cluster_features, default_cluster_labels = np.random.rand(n_samples, 3 if is_3d else 2), None
+        default_cluster_features, default_cluster_labels = np.random.rand(n_samples, n_features), None
     elif data_method == "Varied Variances":
         default_cluster_features, default_cluster_labels = make_blobs(
-            n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], n_features=3 if is_3d else 2, random_state=170
+            n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], n_features=n_features, random_state=170
         )
+    elif data_method == "Anisotropicly distributed":
+        X, y = make_blobs(n_samples=n_samples, n_features=n_features, random_state=170)
+        transformation = [[0.6, -0.6], [-0.4, 0.8]]
+        if is_3d:
+            transformation = [[0.6, -0.6, 0], [0.6, -0.6, 0.6], [-0.4, 0.8, -0.4]]
+        X_aniso = np.dot(X, transformation)
+        default_cluster_features, default_cluster_labels = (X_aniso, y)
 
     x_str = st.sidebar.text_area(
         "x (comma separated numbers)",
@@ -125,36 +138,45 @@ def main():
     else:
         z_str = None
 
-    # Cluster Algo
-    cluster_algo = st.sidebar.selectbox(
-        "Cluster Algorithm", [e.value for e in CLUSTER_ALGORITHMS])
-
-    if cluster_algo == ClusterAlgo.KMEANS:
-        # Cluster Algo Parameters
-        n_clusters = st.sidebar.number_input("Number of Clusters", value=DEFAULT_N_CLUSTERS)
-        cluster_algo_kwargs = {"n_clusters": n_clusters}
-    elif cluster_algo == ClusterAlgo.DBSCAN:
-        eps = st.sidebar.number_input("Epsilon", value=0.5)
-        min_samples = st.sidebar.number_input("Minimum Samples", value=5)
-        cluster_algo_kwargs = {"eps": eps, "min_samples": min_samples}
-    elif cluster_algo == ClusterAlgo.GAUSSIAN_MIXTURE:
-        n_clusters = st.sidebar.number_input("Number of Clusters", value=DEFAULT_N_CLUSTERS)
-        covariance_type = st.sidebar.selectbox(
-            "Covariance type", ["full", "tied", "diag", "spherical"])
-        cluster_algo_kwargs = {"n_components": n_clusters, "covariance_type": covariance_type}
-    else:
-        cluster_algo_kwargs = {}
-
     x, y, z = parse_feature_input(x_str, y_str, z_str)
     if z:
         X = np.column_stack((x, y, z))
     else:
         X = np.column_stack((x, y))
 
-    cluster_labels = get_cluster_labels(X, cluster_algo, **cluster_algo_kwargs)
+    # Cluster Algo
+    cluster_algos: List[str] = st.sidebar.multiselect(
+        "Cluster Algorithms", [e.value for e in CLUSTER_ALGORITHMS], [CLUSTER_ALGORITHMS[0]])
+    display_cols = st.columns(len(cluster_algos))
 
-    # plot the figure
-    st.write(plot_figure(x, y, z, cluster_labels))
+    for i, cluster_algo_str in enumerate(cluster_algos):
+        # Cluster Algo Parameters
+        if cluster_algo_str == ClusterAlgo.KMEANS:
+            st.sidebar.title(ClusterAlgo.KMEANS.value)
+            n_clusters = st.sidebar.number_input("Number of Clusters", value=DEFAULT_N_CLUSTERS)
+            cluster_algo_kwargs = {"n_clusters": n_clusters, "random_state": 1}
+        elif cluster_algo_str == ClusterAlgo.DBSCAN:
+            st.sidebar.title(ClusterAlgo.DBSCAN.value)
+            eps = st.sidebar.number_input("Epsilon", value=0.5)
+            min_samples = st.sidebar.number_input("Minimum Samples", value=5)
+            cluster_algo_kwargs = {"eps": eps, "min_samples": min_samples}
+        elif cluster_algo_str == ClusterAlgo.GAUSSIAN_MIXTURE:
+            st.sidebar.title(ClusterAlgo.GAUSSIAN_MIXTURE.value)
+            n_clusters = st.sidebar.number_input("Number of Clusters", value=DEFAULT_N_CLUSTERS)
+            covariance_type = st.sidebar.selectbox(
+                "Covariance type", ["full", "tied", "diag", "spherical"])
+            cluster_algo_kwargs = {"n_components": n_clusters, "covariance_type": covariance_type}
+        else:
+            cluster_algo_kwargs = {}
+
+        cluster_labels = get_cluster_labels(X, cluster_algo_str, **cluster_algo_kwargs)
+
+        # plot the figure
+        display_cols[i].subheader(cluster_algo_str)
+        fig = plot_figure(x, y, z, cluster_labels)
+        display_cols[i].plotly_chart(fig, use_container_width=True)
+
+
 
 
 if __name__ == "__main__":
